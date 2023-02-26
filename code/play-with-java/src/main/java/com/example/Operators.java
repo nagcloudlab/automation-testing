1245d70e6441a197d72a6bbea72ef14c6a1d7299
Nagabhushanam <nagabhushanam.n@revature.com>
2023-02-26 15:04:55 +0300

    java basics

---
+++ code/play-with-java/src/main/java/com/example/Operators.java
@@ -0,0 +1,25 @@
+package com.example;
+
+public class Operators {
+    public static void main(String[] args) {
+
+        System.out.println(1+1);
+        System.out.println(1-1);
+        System.out.println(1*1);
+        System.out.println(1/1);
+
+        System.out.println(1==1);
+        System.out.println(1!=1);
+        System.out.println(1==1 && 2!=2);
+        System.out.println(1==1 || 2!=2);
+
+        int a=12;
+        int b=a+1;
+
+        System.out.println(a);
+        System.out.println(b);
+
+
+
+    }
+}
---
+++ code/play-with-java/src/main/java/com/example/ValueDataTypes.java
@@ -0,0 +1,35 @@
+package com.example;
+
+public class ValueDataTypes {
+    public static void main(String[] args) {
+
+        /**
+         *
+         *  byte ( 8 )
+         *
+         *  -2^n-1 to 0, 2^n-1 -1
+         *
+         *  -128 to 127
+         *
+         */
+
+        byte byteVar = 12;
+        short shortVar = 1212;
+        int intVar = 1212121212;
+        long longVar = 121212121212121212L;
+
+        double doubleVar=12.12;
+        float floatVar=12.12F;
+
+        char charVar1='A';
+        char charVar2=65; // uni-code
+        char charVar3='\u0041';
+        System.out.println(charVar1);
+        System.out.println(charVar2);
+        System.out.println(charVar3);
+
+        boolean isFound=true;
+
+
+    }
+}
